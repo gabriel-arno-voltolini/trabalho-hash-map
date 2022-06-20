e4501066e77033b383817b06be298bf7a933e063
Gabriel Arno Voltolini <gabriel.voltolini@hbsis.com.br>
2022-06-20 20:41:53 -0300

    Testes adicionados e projeto novo criado

---
+++ tree-sort/src/tests/TreeSortTests.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TreeSortTests {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
